357878795c2cf716a60d93491bd76c33e3adf7d2
avih8r <sparco.209@gmail.com>
2018-01-25 16:12:15 +1100

    complete I think dunno lol

--- generator.py
+++ generator.py
@@ -19,39 +19,22 @@ class generator:
         i = 0
         while i < self.settings.linesOfData: 
             row = self.settings.fields
-            row[0] = random.randint(1,99)
-            row[2] = random.randint(1,99)
-            opNum = random.randint(0,3)
-            if opNum == 0:
-                row[1] = "plus"
-                row[3] = row[0] + row[2]
-            if opNum == 1:
-                row[1] = "minus"
-                row[3] = row[0] - row[2]
-            if opNum == 2:
-                row[1] = "times"
-                row[3] = row[0] * row[2]
-            if opNum == 3:
-                row[1] = "divide"
-                if row[0] == 0:
-                    row[0] = random.randint(1,99)
-                if row[2] == 0:
-                    row[2] = random.randint(1,99)
-                row[3] = row[0] / row[2]
+            self.parseRow
             #print(row)
             writer.writerow(row)
-            i+=1
+            i+=1            
         ofile.close()
+        
     def parseRow(self):
-        print(stub)
+        self.randomiseFields(row,(1,999))
         
-    def randomiseField(self, row, field, range, format)
+    def randomiseField(self, row, field, range):
+        row[fieldsToRand[field]]=random.randint(range)
         
-    def randomiseFields(self, row, range, format):
-        for value in row:
-            if value
-            random.randint(range)
-        print(stub)
+    def randomiseFields(self, row, range):
+        for value in fieldsToRand:
+            row[fieldsToRand] = random.randint(range)
         
-    def calculateField(self, row, field):
-        print(stub)
+    def calculateField(self, row, *field):
+        #add your own algorithm here
+        row[fieldsToRand[field[2]]] = row[fieldsToRand[field[0]]] + row[fieldsToRand[field[1]]]
